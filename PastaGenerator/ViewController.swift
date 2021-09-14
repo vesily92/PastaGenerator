97f5d4e9cfee6fcfd63028f5ffda928761a8ce40
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-14 10:08:42 +0300

    Initial Commit

---
+++ PastaGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PastaGenerator
+//
+//  Created by Василий Пронин on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
